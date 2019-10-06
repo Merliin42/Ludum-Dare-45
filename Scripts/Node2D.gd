9185ea7b9933ca02306b4ae7851f532cd55f677c
Lucas <lucasdupuis7@gmail.com>
2019-10-06 12:33:10 +0200

    Correction

--- Scripts/Node2D.gd
+++ Scripts/Node2D.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 onready var nav_2d : Navigation2D = $Navigation2D
-onready var line_2d : Line2D = $Line2D
 onready var mob : Sprite = $Mob
 
 func _unhandled_input(event : InputEvent) -> void:
