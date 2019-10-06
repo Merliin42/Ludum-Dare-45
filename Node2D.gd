459829f8fe69641d98554b68ad22457fc9a338ac
Lucas <lucasdupuis7@gmail.com>
2019-10-06 11:20:02 +0200

    Début nav

---
+++ Node2D.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+onready var nav_2d : Navigation2D = $Navigation2D
+onready var line_2d : Line2D = $Line2D
+onready var character : Sprite = 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
